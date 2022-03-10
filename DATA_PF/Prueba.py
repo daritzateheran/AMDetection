7de04e36eeb00a8230079c4dfb2068305bf0d7a8
Xaeina <67559845+xaiena@users.noreply.github.com>
2022-03-10 01:21:19 -0500

    enviroment

---
+++ DATA_PF/Prueba.py
@@ -0,0 +1,33 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+from sklearn.cluster import DBSCAN
+
+name=os.getenv('NAME_')
+name=name.split(",")
+#path=[r"C:Blow_nose",r"C:Brush_hair", r"C:Drink_water"]
+df = pd.DataFrame()
+k = 0
+#os.getcwd()
+
+path=os.getenv('PATH_')
+for p in name:
+    os.chdir(path)
+    os.chdir(p)
+    file_extension = '.csv'
+    all_filenames = [i for i in glob.glob(f"*{file_extension}")]
+    for f in all_filenames:
+        temp = pd.read_csv(f, names=['x', 'y', 'z'])
+        temp['sample'] = k
+        temp['activity'] = f'{p[2:]}'
+        df = df.append([temp], ignore_index=True)
+        k = k + 1
+V=[]
+for i in range(k):
+    sp = df[df['sample']==i]
+    sp = sp.drop(['activity'], axis=1)
+    #sp.describe
+    V.append(sp.values)
+print(V)
+#db = DBSCAN(eps=0.3, min_samples=10).fit(V)
